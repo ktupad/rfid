47eac1705fedd08169acdbce1696557a757aca72
ktupad <30571927+ktupad@users.noreply.github.com>
2019-09-23 02:06:54 +0700

    Update app.php

--- app.php
+++ app.php
@@ -5,10 +5,10 @@ error_reporting(E_ALL);
   include('ktupad/ktupad.php');
   class app extends mod {
   public $local= array(
-    's' => 'localhost',
-    'u' => 'ktupad',
-    'p' => '1234',
-    'n'=> 'demo'
+    's' => 'den1.mysql2.gear.host',
+    'u' => 'rfidktupad',
+    'p' => 'Go1VX3-~7CR5',
+    'n'=> 'rfidktupad'
     );
   }
 
